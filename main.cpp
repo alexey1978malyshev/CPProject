6a8e28db51ac159aeb6ce8572ba0670218f3f9b4
Alexey <alexey1978malyshev@gmail.com>
2021-09-30 11:02:19 +0000

    количесво ребер в вершине графа

--- main.cpp
+++ main.cpp
@@ -1,5 +1,26 @@
 #include <iostream>
+#include <vector>
 using namespace std;
+
+
 int main() {
-  cout << "Hello World!\n";
+  int n = 5, m = 12;
+  cin >> n >> m;
+  
+  vector<int> crosses(n);
+
+  for (int i = 0; i < n; ++i)
+  {
+    int from, to;
+    cin >> from >> to; 
+    crosses [from-1]++;
+    crosses [to-1]++;
+  }
+
+  for (int cross : crosses)
+  {
+    cout << cross << " ";
+  }
+  return 0;
+
 }
